a53cc28a9f21277eb4802fac6fe8f1e8b94ff3d9
OMOTO Kenji <doloopwhile@gmail.com>
2015-05-18 19:33:44 +0900

    tmp

--- app.go
+++ app.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"encoding/gob"
+	"fmt"
 	"net/http"
+	"os"
 	"text/template"
 
-	"github.com/gorilla/context"
-	"github.com/gorilla/securecookie"
-	"github.com/gorilla/sessions"
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	// "github.com/gorilla/securecookie"
+	"github.com/goincremental/negroni-sessions"
+	"github.com/goincremental/negroni-sessions/cookiestore"
 	"github.com/jameskeane/bcrypt"
-	"github.com/zenazn/goji"
-	"github.com/zenazn/goji/web"
 )
 
 type (
@@ -24,28 +27,30 @@ var (
 	tmpl  *template.Template
 )
 
-func getSession(c web.C) *sessions.Session {
-	return c.Env["Session"].(*sessions.Session)
+func init() {
+	gob.Register(User{})
 }
-
-func logout(c web.C, w http.ResponseWriter, r *http.Request) {
-	session := getSession(c)
-	session.Values["User"] = nil
+func logout(w http.ResponseWriter, r *http.Request) {
+	session := sessions.GetSession(r)
+	session.Set("User", nil)
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
-func register(c web.C, w http.ResponseWriter, r *http.Request) {
-	session := getSession(c)
+func register(w http.ResponseWriter, r *http.Request) {
+	session := sessions.GetSession(r)
 
-	c.Env["Flashes"] = session.Flashes("auth")
-	err := tmpl.ExecuteTemplate(w, "register.tmpl", c.Env)
+	data := map[string]interface{}{
+		"User":    session.Get("User"),
+		"Flashes": session.Flashes("auth"),
+	}
+	err := tmpl.ExecuteTemplate(w, "register.tmpl", data)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func registerPost(c web.C, w http.ResponseWriter, r *http.Request) {
-	session := getSession(c)
+func registerPost(w http.ResponseWriter, r *http.Request) {
+	session := sessions.GetSession(r)
 
 	r.ParseForm()
 	name, password := r.FormValue("user_name"), r.FormValue("password")
@@ -53,7 +58,7 @@ func registerPost(c web.C, w http.ResponseWriter, r *http.Request) {
 	for _, user := range users {
 		if user.Name == name {
 			session.AddFlash("user is already registered", "auth")
-			register(c, w, r)
+			register(w, r)
 			return
 		}
 	}
@@ -66,37 +71,39 @@ func registerPost(c web.C, w http.ResponseWriter, r *http.Request) {
 	}
 
 	users = append(users, user)
-	c.Env["User"] = user
-
+	session.Set("User", user)
+	fmt.Println(session.Get("User"))
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
-func index(c web.C, w http.ResponseWriter, r *http.Request) {
-	err := tmpl.ExecuteTemplate(w, "index.tmpl", c.Env)
+func index(w http.ResponseWriter, r *http.Request) {
+	session := sessions.GetSession(r)
+
+	data := map[string]interface{}{"User": session.Get("User")}
+	fmt.Println(session.Get("User"))
+	err := tmpl.ExecuteTemplate(w, "index.tmpl", data)
 	if err != nil {
 		panic(err)
 	}
 }
 
 func main() {
+	n := negroni.Classic()
 	tmpl = template.Must(template.ParseGlob("templates/*.tmpl"))
 
-	key := securecookie.GenerateRandomKey(10)
-	sessionStore := sessions.NewCookieStore(key)
+	sessionStore := cookiestore.New([]byte("secret123"))
+	n.Use(sessions.Sessions("login", sessionStore))
 
-	goji.Use(func(c *web.C, h http.Handler) http.Handler {
-		fn := func(w http.ResponseWriter, r *http.Request) {
-			session, _ := sessionStore.Get(r, "session")
-			c.Env["Session"] = session
-			h.ServeHTTP(w, r)
-			context.Clear(r)
-		}
-		return http.HandlerFunc(fn)
-	})
+	r := mux.NewRouter()
+	r.HandleFunc("/", index).Methods("GET")
+	r.HandleFunc("/logout", logout).Methods("GET")
+	r.HandleFunc("/register", register).Methods("GET")
+	r.HandleFunc("/register", registerPost).Methods("POST")
+	n.UseHandler(r)
 
-	goji.Get("/", index)
-	goji.Get("/logout", logout)
-	goji.Get("/register", register)
-	goji.Post("/register", registerPost)
-	goji.Serve()
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "3000"
+	}
+	n.Run(":" + port)
 }
